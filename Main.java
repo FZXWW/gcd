845c0f497aeae1d9ca7ba7683afeb3f97202f060
Mohammad <dr.fmfm2004@gmail.com>
2024-11-24 23:13:20 +0300

    create a new line to see it in github

--- Main.java
+++ Main.java
@@ -4,6 +4,8 @@ public class Main {
 
     public static void main(String[] args) {
 
+        System.out.println("hello from GITHUB");
+
         try {
             Scanner input = new Scanner(System.in);
 
